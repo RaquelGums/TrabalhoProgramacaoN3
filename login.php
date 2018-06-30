937691b413ff3c80d8a896f2feab433983315f6b
m19u31 <m19u31@outlook.com>
2018-06-29 19:54:47 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -1,30 +1,44 @@
 <!DOCTYPE>
+<?php session_start(); ?>
 <html>
-	<head>
-	<form>
-	Login: <input type="text" name="login"><br>
-	<br>
-	Senha: <input type="password" name="senha">
+	<head style="align:center;" >
+    	<form action="login.php" method="post">
+	email: <input type="text" name="email"><br>
+	<br><br>
+	Senha: <input type="password" name="senha"><br>
 	<input type="submit" value="Entrar">
+	<input type="button" value="cadastrar" onClick='location.href="cadastroUsuario.php"'>
+	<input type="button" value="esqueci a senha" onClick='location.href="recuperarSenha.php"'>
 	</form>
+	
 	</head>
 	<body>
 	<?php
-		if(!empty($_GET)){
-			if(!empty($_GET['login'])){
-			$senha=$_GET['login'];
+		if(!empty($_POST)){
+			if(!empty($_POST['email'])){ $email=$_POST['email'];}
+			if(!empty($_POST['senha'])){ $senha=$_POST['senha'];}
+			// linhas responsáveis em se conectar com o bando de dados.
+			$db= new PDO('mysql:host=localhost;dbname=n3;charset=utf8','root','');
+			//$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			//  faz uma pesquisa na tabela de usuarios
+			$r=$db->prepare("SELECT idUsuario FROM usuario WHERE emailUsuario=:email and senhaUsuario=:senha");
+			$r->execute(array(':email'=>$email,':senha'=>$senha));
+			$linhas=$r->fetchAll(PDO::FETCH_NUM);
+
+			// se ela estiver encontrado algum registro vai criar sessão
+			if(!empty($linhas))
+			{	
+				$_SESSION['email'] = $email;
+				$_SESSION['senha'] = $senha;
+				header('location:telainicial.php');
 			}
-			if(!empty($_GET['senha'])){
-			$senha=$_GET['senha'];
-			}
-			if ($login=='admin'){
-				if ($senha==12345){
-					echo "teste";
-				}
-			}
-			else {
-				echo "Dados Inválidos!";
-			}
+			else{
+    			unset ($_SESSION['email']);
+				unset ($_SESSION['senha']);
+				header('location:login.php');
+			} 
+		}else {
+			echo("vazio");
 		}
 	?>
 	</body>
