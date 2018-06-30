d97d2bb50459ba1c32ab10b3ea293174f59865c3
RaquelGums <40545665+RaquelGums@users.noreply.github.com>
2018-06-29 21:44:58 -0300

    senha

---
+++ alterarSenha.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html>
+	<head>
+	<style>
+		body {
+			background-image: url(fundo.jpg);
+		}
+	</style>
+	</head>
+	<body>
+		<form id="area" method="get">			
+			<fieldset>
+				<legend>Alteração de Senha</legend>
+				Email: <input type="text" name="email"><br>
+				<br>Senha Atual: <input type="text" name="senha"><br>
+				<br>Nova Senha: <input type="text" name="senhaNova"><br>
+				<br><input type="submit" value="Alterar">
+				<button value="cancelar">Cancelar
+			</fieldset>
+		</form>
+	</body>
+</html>
---
+++ recuperarSenha.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+	<head>
+	<style>
+		body {
+			background-image: url(fundo.jpg);
+		}
+	</style>
+	</head>
+	<body>
+		<form id="area" method="get">
+				
+			<fieldset>
+				<legend>Recuperação de Senha</legend>
+				Email: <input type="text" name="email"><br>
+				<br><input type="submit" value="Recuperar">
+				<button value="cancelar">Cancelar
+			</fieldset>
+		</form>
+	</body>
+</html>
